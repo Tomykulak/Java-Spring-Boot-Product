a6a668f08bbb72eb912ba1fef5c7f54a186d7b9c
Tomáš Okruhlica <tomasokruhlica@seznam.cz>
2024-11-07 17:13:51 +0100

    Spring Boot Initialzr

---
+++ services/src/test/java/tomykulak/services/TestProductApplication.java
@@ -0,0 +1,11 @@
+package tomykulak.services;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ProductApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
